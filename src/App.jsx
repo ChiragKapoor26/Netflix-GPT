ceb0ca6de7fcc5004978ad7de5cc8c59368171b8
Chirag kapoor <chiragkapoor80@gmail.com>
2025-06-23 01:32:33 +0530

    Implement the Header and Sign in , Sign Out Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import Body from "./components/Body";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="">Namste Everyone we are going to build the netflix gpt.</h1>
-    </div>
+    <Body/>
   )
 }
 export default App;
---
+++ src/components/Browser.jsx
@@ -0,0 +1,8 @@
+const Browser = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">This is a Browser Component</h1>
+        </div>
+    )
+}
+export default Browser;
