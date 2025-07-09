c0f0acc4c612e85513613eb099151035f412659d
Chirag kapoor <chiragkapoor80@gmail.com>
2025-07-10 02:44:27 +0530

    fix bug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const API_options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: "Bearer"+import.meta.env.VITE_TMDB_API_KEY;
+    Authorization: "Bearer"+import.meta.env.VITE_TMDB_API_KEY
   }
 };
 export const IMG_CDN = "https://image.tmdb.org/t/p/w500";
