ab94b1ab39a1dc64c8bed5a3770207fe0da1c168
Chirag kapoor <chiragkapoor80@gmail.com>
2025-07-09 15:52:54 +0530

    Implement Filtered movie data

---
+++ src/components/GptMovieCard.jsx
@@ -0,0 +1,13 @@
+import { IMG_CDN } from "../utils/constants";
+const GptMovieCard = ({posterPath,title}) => {
+    if(!posterPath) return null;
+    return (
+        <div className="w-[300px] cursor-pointer bg-gray-700 opacity-80 rounded-lg p-2 flex flex-col items-center">
+            <img alt="Movie-Card" src={IMG_CDN+posterPath} className="h-[80%] w-[90%] rounded-lg"/>
+            <div className="h-[20%] w-[90%] flex justify-center items-center text-white">
+                <h1 className="font-bold text-xl text-center">{title}</h1>
+            </div>
+        </div>
+    )
+}
+export default GptMovieCard;
--- src/components/GptMovieSuggestion.jsx
+++ src/components/GptMovieSuggestion.jsx
@@ -1,15 +1,20 @@
+import GptMovieCard from "./GptMovieCard";
 import MovieList from "./MovieList";
 import { useSelector } from "react-redux";
 const GptMovieSuggestion = () => {
     //Subscribing to the store and getting the data
     const movieResults = useSelector(store => store.Gpt.gptMovies);
     const movieNames= useSelector(store => store.Gpt.movieNames);
+    console.log(movieResults);
     // if(!movieNames||!movieResults) return <p>Loading..</p>;
-    return movieNames&&movieResults && (
-        <div className="p-4 m-4 bg-black text-white bg-opacity-90">
-            {movieNames.map((movie,index) =>(
-                <MovieList key={movie} title={movie} movies={movieResults[index]}/>
-            ) )}
+    return movieResults&&(
+        <div className="p-4 justify-center m-4 bg-black bg-opacity-90 flex gap-x-20 gap-y-10 flex-wrap">
+            {movieResults.map((movieArray) =>{
+                const firstMovie = movieArray[0];
+                return firstMovie ? (
+                    <GptMovieCard key={firstMovie.id} posterPath={firstMovie.poster_path} title={firstMovie.title}/>
+                ):null;
+ } )}
         </div>
     )
 }
